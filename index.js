cbe5c2f8115538a6d5170c4952b59a2adb1ceb20
gangasrotogati <glass_joseph@hotmail.com>
2022-01-21 15:02:18 -0800

    Joe and Kimiya Homework 3

--- index.js
+++ index.js
@@ -4,28 +4,74 @@ document.addEventListener("DOMContentLoaded", function (event) {
    
 
     document.getElementById("show").addEventListener("click", function () {
-        console.log(MovieArray);
-        let message = "List of movies\n";
-        for (i = 0; i < MovieArray.length; i++) {
-            message = message + MovieArray[i].movie + "  " + MovieArray[i].rate+ "  " + MovieArray[i].year + "\n";
+        let ul = document.createElement('ul');
+        let movieList = document.getElementById("output").appendChild(ul);
+
+        for (const movie of MovieArray1) {
+            SortedMovieArray.push(movie)
         }
-        document.getElementById("output").value = message;
+        for (const movie of MovieArray2) {
+            SortedMovieArray.push(movie)
+        }
+        for (const movie of MovieArray3) {
+            SortedMovieArray.push(movie)
+        }
+        for (const movie of MovieArray4) {
+            SortedMovieArray.push(movie)
+        }
+        for (const movie of MovieArray5) {
+            SortedMovieArray.push(movie)
+        }
+
+        movieList.innerHTML = "<h2>List of movies</h>"
+        console.log(SortedMovieArray);
+        for (i = 0; i < SortedMovieArray.length; i++) {
+            movieList.innerHTML += "<li>" + SortedMovieArray[i].movie + " " + SortedMovieArray[i].rate + " " + SortedMovieArray[i].year + "</li>";
+        }
+
     });
 });
 
-MovieArray = []; 
+let movieCount = 0;
+let MovieArray1 = []; 
+let MovieArray2 = [];
+let MovieArray3 = [];
+let MovieArray4 = [];
+let MovieArray5 = [];
+let SortedMovieArray = [];
 
 function formSubmitEvent() {
+
+    movieCount ++;
+
     let Movie = {
         movie:  document.getElementById("movie1").value,
-        rate: document.getElementById("rate").value,
+        rate: Number(document.getElementById("rate").value),
         year: document.getElementById("year").value,
-       
     }
-    
 
-    MovieArray.push(Movie);
-    
+    if(Movie.rate == "" || Movie.rate < 1 || Movie.rate > 5){
+        alert("Error, undefined rating. Please rate between 1 and 5.")
+    }
+
+    if(movieCount < 11 ){
+
+        if(Movie.rate === Number(1)){
+            MovieArray1.push(Movie);
+        }
+        if(Movie.rate === Number(2)){
+            MovieArray2.push(Movie);
+        }
+        if(Movie.rate === Number(3)){
+            MovieArray3.push(Movie);
+        }
+        if(Movie.rate === Number(4)){
+            MovieArray4.push(Movie);
+        }
+        if(Movie.rate === Number(5)){
+            MovieArray5.push(Movie);
+        }
+    }
  
     document.getElementById("movie").value = Movie.rate;
   
